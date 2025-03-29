c34ca21c814dcdabec8e71d92dcdc16dd2625e98
Lochan-sharma232 <lochansharma537@gmail.com>
2025-03-29 13:16:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function toCase(text) {
+	let string1= " ";
+	console.log(string.tolowercase());
+	
   // write your code here
 }
-
 // DO not change the code below
 
-const text = prompt("Enter text:");
-alert(toCase(text));
+// const text = prompt("Enter text:");
+// alert(toCase(text));
