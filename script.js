2447681d350d99cb4dc25b7080c34144c18e1b4b
Lochan-sharma232 <lochansharma537@gmail.com>
2025-03-29 15:12:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toCase(text) {
 	let string1= " ";
-	console.log(string.tolowercase());
+	console.log(string1.toLowercase()-string1.toUpperCase());
   // write your code here
 }
 // DO not change the code below
